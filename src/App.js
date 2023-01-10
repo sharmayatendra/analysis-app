f009dc496cc4be1f34b921f83f4bf7f1b7fc4502
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2023-01-11 02:49:13 +0530

    charts added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
+import ReactEcharts from "echarts-for-react";
+import Winedata from "./Wine-Data.json";
 function App() {
+  const scatterPlot = Winedata.map((item) => [
+    item["Color intensity"],
+    item["Hue"],
+  ]);
+
+  let bardata = {
+    avg1: 0,
+    length1: 0,
+    avg2: 0,
+    length2: 0,
+    avg3: 0,
+    length3: 0,
+  };
+
+  for (let i = 0; i < Winedata.length; i++) {
+    if (Winedata[i]["Alcohol"] === 1) {
+      bardata.length1++;
+      bardata.avg1 += Winedata[i]["Malic Acid"];
+    } else if (Winedata[i]["Alcohol"] === 2) {
+      bardata.length2++;
+      bardata.avg2 += Winedata[i]["Malic Acid"];
+    } else {
+      bardata.length3++;
+      bardata.avg3 += Winedata[i]["Malic Acid"];
+    }
+  }
+
+  bardata.avg1 = bardata.avg1 / bardata.length1;
+  bardata.avg2 = bardata.avg2 / bardata.length2;
+  bardata.avg3 = bardata.avg3 / bardata.length3;
+
+  const option = {
+    xAxis: {
+      name: "Color intensity",
+    },
+    yAxis: {
+      name: "Hue",
+    },
+    series: [
+      {
+        data: scatterPlot,
+        type: "scatter",
+      },
+    ],
+  };
+
+  const barOptions = {
+    xAxis: {
+      name: "Alcohol",
+      data: ["1", "2", "3"],
+    },
+    yAxis: {
+      name: "Malic Acid",
+    },
+    series: [
+      {
+        data: [bardata.avg1, bardata.avg2, bardata.avg3],
+        type: "bar",
+        barWidth: "20%",
+        barGap: "10%",
+      },
+    ],
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ReactEcharts option={option} />
+      <ReactEcharts option={barOptions} />
+    </>
   );
 }
 
